aa209b710e5cdae83813f77e8f078b24bdc682c2
PiotMac <268425@student.pwr.edu.pl>
2022-12-15 16:19:26 +0100

    repairing test

--- src/test/java/MoveAvailabilityTest.java
+++ src/test/java/MoveAvailabilityTest.java
@@ -2,10 +2,10 @@ import org.example.CheckersBoard;
 import org.junit.Test;
 
 public class MoveAvailabilityTest {
-    CheckersBoard checkersBoard = new CheckersBoard();
+    CheckersBoard checkersBoard = new CheckersBoard(10);
     @Test
     public void basicMoveTest() {
-        checkersBoard.createBoard();
+        checkersBoard.getBoard();
         CheckersBoard.board[0][1].piece.checkLegalMoves();
         CheckersBoard.board[3][0].piece.checkLegalMoves();
         CheckersBoard.board[3][4].piece.checkLegalMoves();
